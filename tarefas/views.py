a32a2eb79b47c0d16eaefe2b8cdfc168ceebf6e2
AdanDeFaria <adanpaulo12@gmail.com>
2023-04-02 19:48:28 -0300

    commit

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-
+import pandas as pd
+import os
 from django.http import HttpResponse
 # Create your views here.
+usuario = os.environ['USERPROFILE']
+print(usuario)
+path = os.path.join(usuario, 'Documents', 'github', 'AFRepository', 'projeto.xlsx')
+projetos = pd.read_excel(path, sheet_name="projetos")
+projetos = projetos.loc[projetos['status'] == "ativo", 'projetos']
+projetos = projetos.values.tolist()
+
+funcionarios = pd.read_excel(path, sheet_name="funcionarios")
+funcionarios = funcionarios.loc[funcionarios['status'] == "ativo", 'funcionarios']
+funcionarios = funcionarios.values.tolist()
+
+funcionarios = pd.read_excel(path, sheet_name="funcionarios")
+funcionarios = funcionarios.loc[funcionarios['status'] == "ativo", 'funcionarios']
+funcionarios = funcionarios.values.tolist()
+print(funcionarios)
+
 context = {
-    'employees' : ["Employee A", "Employee B", "Employee C"],
-    'projects' : ["Project X", "Project Y", "Project Z"],
+    'employees' : funcionarios,
+    'projects' : projetos,
     'tasks' : ["Task 1", "Task 2", "Task 3"],
     'requesters' : ["Requester 1", "Requester 2", "Requester 3"]
 }
