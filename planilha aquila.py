549f8f51ccbc215add91aaf004762e8a1c71e4b0
AdanDeFaria <adanpaulo12@gmail.com>
2023-04-05 09:13:32 -0300

    commit

--- planilha aquila.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r'C:\Users\gsi-Adan\Documents\Aquila\Por_Equipe.csv',
-                 encoding="UTF-8",
-                 sep=";")
-print(df)
-df.to_excel(r'C:\Users\gsi-Adan\Documents\Aquila\Por_Equipe.xlsx',
-            index=False)
-
